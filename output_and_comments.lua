b81f41fc08a63a7ec0198418ad022aa149eba33b
wizpizz <wizongame@gmail.com>
2022-02-25 12:15:06 +0100

    Initial release

---
+++ output_and_comments.lua
@@ -0,0 +1,13 @@
+-- prints text to the screen
+
+print("Hello World!") -- prints text to the screen
+print("Oi")
+
+--[[
+    This program
+    is a basic program
+    to display some text on the screen
+    print("Hello World!")
+]]
+
+print("Hello " .. "Jack" .. "!" .. " My name is Steve") -- concatenates multiple strings
