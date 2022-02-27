fb65a395b898ceb775dc4b1915e5ee9044f680e6
wizpizz <wizongame@gmail.com>
2022-02-27 18:33:22 +0100

    Added custom modules and mymath

---
+++ custom_modules.lua
@@ -0,0 +1,4 @@
+local mod = require("mymath")
+
+print(mod.add(5, 10))
+print(mod.power(2, 5))
---
+++ mymath.lua
@@ -0,0 +1,11 @@
+_G.mmath = {}
+
+function mmath.add(x, y) -- add the add function to the mmath table
+    return x + y
+end
+
+function mmath.power(num1, num2)
+    return num1 ^ num2
+end
+
+return mmath
